fe372f582b9ab5e53c5d4917f74b4d9759dbb918
MD SALMAN  BISWAS <mdsalmanhelp0@gmail.com>
2026-01-19 06:26:07 +0600

    Create __init__.py

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,17 @@
+from . import admin
+from . import channel
+from . import music
+from . import moderation
+from . import analytics
+from . import utility
+from . import auto_reply
+
+__all__ = [
+    'admin',
+    'channel',
+    'music',
+    'moderation',
+    'analytics',
+    'utility',
+    'auto_reply',
+]
